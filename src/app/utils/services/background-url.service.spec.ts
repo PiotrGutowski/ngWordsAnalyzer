4d86f03ff8f4c4bf9370e14bf14dd7d479f873bb
Piotr Gutowski <piotr.gutowski@cybercom.com>
2020-04-03 20:36:58 +0200

    getYouTubeSelectedMovieId unit test

--- src/app/utils/services/background-url.service.spec.ts
+++ src/app/utils/services/background-url.service.spec.ts
@@ -13,4 +13,10 @@ describe('BackgroundUrlService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('getYouTubeSelectedMovieId should return proper id', () => {
+    const youTubeUrl = 'https://www.youtube.com/watch?v=fM_jWSfOUOo';
+    const youTubeId = 'fM_jWSfOUOo';
+    expect(service.getYouTubeSelectedMovieId(youTubeUrl)).toEqual(youTubeId);
+  });
 });
